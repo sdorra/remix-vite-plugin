3a27de9de1ebdf027ffb26eb311ef7a9cc3eecbb
Sebastian Sdorra <s.sdorra@gmail.com>
2024-01-22 19:07:46 +0100

    Add sample plugin to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+function viteSamplePlugin(): Plugin {
+  console.log("vsp: create plugin");
+  return {
+    name: "vite-sample-plugin",
+    configResolved() {
+      console.log("vsp: configResolved");
+    },
+    buildStart() {
+      console.log("vsp: buildStart");
+    },
+    configureServer() {
+      console.log("vsp: configureServer");
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths(), viteSamplePlugin()],
 });
